e1142b7b85ec4ccdd3079c1135a1a573c6a402cc
Gustominox <agstcmps.111@gmail.com>
2025-06-15 23:33:21 +0100

    feat: Add dir for CSC and CSR omp implementation

--- run_tests.sh
+++ run_tests.sh
@@ -13,7 +13,8 @@ dirs=(
     # "WA2/"
     # "WA2-Vectorize/"
     # "WA2-CSC/"
-    "WA2-CSC-CSR/"
+    # "WA2-CSC-CSR/"
+    "WA2-CSC-CSR-OpenMP/"
     # "WA2-CSC-OpenMP/"
     # "WA2-CSC-OpenMP-V2/"
     # "WA2-Vectorize-OpenMP/"
@@ -42,6 +43,28 @@ for dir in "${dirs[@]}"; do
         elif [[ "$dir" == "WA2-CSC-CSR/" ]]; then
         sbatch --exclusive ./WA2-CSC-CSR/perf.sh WA-CSC-CSR ${SIZE}
             
+
+    # 
+        elif [[ "$dir" == "WA2-CSC-CSR-OpenMP/" ]]; then
+            # sbatch --exclusive ./$dir/perf.sh WA-OpenMP-16threads ${SIZE} 16
+            for threads in 2 4 8 16 32; do # 64 96 128; do
+                echo "Running with ${threads} threads..."
+                for run in {1..1}; do
+                    echo "Run ${run} for ${threads} threads..."
+                    sbatch --exclusive ./WA2-CSC-CSR-OpenMP/perf.sh WA-CSC-CSR-OpenMP-${threads}threads-${run} ${SIZE} ${threads}
+                done
+                # sbatch --exclusive ./WA2-CSC-OpenMP/perf.sh WA-CSC-OpenMP-${threads}threads ${SIZE} ${threads}
+            done
+
+            for threads in 2 4 8 16 32 48; do
+                echo "Running with ${threads} threads..."
+                for run in {1..1}; do
+                    echo "Run ${run} for ${threads} threads..."
+                    sbatch --exclusive ./WA2-CSC-CSR-OpenMP/perf_arm.sh WA-CSC-CSR-OpenMP-Arm-${threads}threads-${run} ${SIZE} ${threads}
+                done
+                # sbatch --exclusive ./WA2-CSC-OpenMP/perf.sh WA-CSC-OpenMP-${threads}threads ${SIZE} ${threads}
+            done
+
 
         elif [[ "$dir" == "WA2-CSC-OpenMP/" ]]; then
             # sbatch --exclusive ./$dir/perf.sh WA-OpenMP-16threads ${SIZE} 16
